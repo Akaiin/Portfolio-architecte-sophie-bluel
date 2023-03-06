a253a9da376a45216bf5f460c122549da280126d
Akain <akain1612@gmail.com>
2023-03-06 16:31:35 +0100

    add delete images buttons

--- FrontEnd/assets/scripts/app.js
+++ FrontEnd/assets/scripts/app.js
@@ -3,12 +3,14 @@ const modalContainer = document.querySelector('.modal')
 const modalContent = document.querySelector('.modal-content')
 const modalImages = document.querySelector('.modal__images__container')
 const loginLink = document.querySelector('.login')
+const closeBtn = document.querySelector('#close--btn')
 
-fetch('http://localhost:5678/api/works')
+fetch('http://localhost:5678/api/works') /* récupération des travaux */
     .then((response) => {
         return response.json()
     })
     .then((data) => {
+        /* ajouts des travaux dans la page d'accueil */
         data.forEach((image) => {
             const figure = document.createElement('figure')
             const figureCaption = document.createElement('figcaption')
@@ -20,43 +22,61 @@ fetch('http://localhost:5678/api/works')
             imagesContainer.appendChild(figure)
             figure.appendChild(figureImage)
             figure.appendChild(figureCaption)
-
-            figureImage.addEventListener('click', (event) => {
-                modalContainer.style.display = 'flex'
-
-                modalImages.innerHTML = ''
-                data.forEach((image) => {
-                    const modalCard = document.createElement('div')
-                    const modalPhotos = document.createElement('img')
-                    const deleteBtn = document.createElement('button')
-
-                    modalCard.classList.add('modal__images__card')
-                    modalPhotos.src = image.imageUrl
-                    modalPhotos.alt = image.title
-                    modalImages.appendChild(modalCard)
-                    modalCard.appendChild(modalPhotos)
-                    modalPhotos.classList.add('modal__images-size')
-                    modalCard.appendChild(deleteBtn)
+            /* si l'utilisateur est connecté, création de l'option log out */
+            if (sessionStorage.user) {
+                loginLink.innerHTML = 'logout'
+                loginLink.style.fontWeight = 700
+                loginLink.addEventListener('click', (event) => {
+                    event.preventDefault()
+                    sessionStorage.removeItem('user')
+                    location.href = 'http://127.0.0.1:5500/FrontEnd/index.html'
                 })
-            })
+                /* création de la modale */
+                figureImage.addEventListener('click', (event) => {
+                    modalImages.innerHTML = ''
+                    modalContainer.style.display = 'flex'
+
+                    closeBtn.addEventListener('click', (event) => {
+                        modalContainer.style.display = 'none'
+                    })
+                    data.forEach((image) => {
+                        const modalCard = document.createElement('div')
+                        const modalPhotos = document.createElement('img')
+                        const deleteBtn = document.createElement('button')
+
+                        modalCard.classList.add('modal__images__card')
+                        modalPhotos.src = image.imageUrl
+                        modalPhotos.alt = image.title
+                        modalImages.appendChild(modalCard)
+                        modalCard.appendChild(modalPhotos)
+                        modalPhotos.classList.add('modal__images-size')
+                        modalCard.appendChild(deleteBtn)
+                        /* suppréssion des travaux lors du click sur les boutons */
+                        deleteBtn.addEventListener('click', (event) => {
+                            event.preventDefault()
+                            const token = JSON.parse(sessionStorage.getItem('user')).token
+                            fetch(`http://localhost:5678/api/works/${image.id}`, {
+                                method: 'DELETE',
+                                headers: {
+                                    Accept: 'application/json',
+                                    Authorization: `Bearer ${token}`,
+                                },
+                            })
+                        })
+                    })
+                })
+            }
         })
     })
 
+/* fermeture de la modale */
 modalContainer.addEventListener('click', (event) => {
     if (event.target == modalContainer) {
         modalContainer.style.display = 'none'
     }
 })
 
+/* redirection vers la page de connexion lors du click sur login */
 loginLink.addEventListener('click', (event) => {
     location.href = 'http://127.0.0.1:5500/FrontEnd/login.html'
 })
-
-if (sessionStorage.user) {
-    loginLink.innerHTML = 'log off'
-    loginLink.addEventListener('click', (event) => {
-        event.preventDefault()
-        sessionStorage.removeItem('user')
-        location.href = 'http://127.0.0.1:5500/FrontEnd/index.html'
-    })
-}
